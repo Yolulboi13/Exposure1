fd73db3c88a961b357bc32073a52c1c4a4527715
Yolulboi13 <marcus.debarbieri@vtsd.com>
2025-05-27 13:55:11 -0400

    5/27/25

---
+++ yuh/HelloWorld.java
@@ -0,0 +1,10 @@
+package yuh;
+
+public class HelloWorld {
+    // This is how comments work
+    // This thign creates the method main
+    public static void main(String[] args) {
+        // This is how printing works
+        System.out.println("Yo what's goodie my gangalang?");
+    }
+}
---
+++ yuh/VariableIntro.java
@@ -0,0 +1,21 @@
+package yuh;
+
+public class VariableIntro {
+    public static void main(String[] args) {
+        /* multi
+        * line
+        * comments
+        * are
+        * pretty
+        * chill
+        * ngl
+         */
+        // datatype name = value;
+        double salary = 277353.0;
+        int raise = 2;
+        // casting a variable is changing the datatpye
+        raise = (int) salary;
+        System.out.println(raise);
+        System.out.println(salary);
+    }
+}
